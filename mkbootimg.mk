521d8103840752223ace6e5a4557d22dfda70abb
zombah <zombah@gmail.com>
2015-05-13 14:16:03 +0300

    mkbootimg: Create recovery with version suffix
    Change-Id: Ia9951424e76733888ea875575d6f4b3109835496
    Signed-off-by: zombah <zombah@gmail.com>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -50,4 +50,5 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) cp $@ $(PRODUCT_OUT)/recovery-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d).img
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
