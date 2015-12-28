4dd6360fd22bd2a8c2a23798195e8498b63e665f
Misha Komarovskiy <zombah@gmail.com>
2015-12-29 01:56:49 +0300

    wifi: New style wifi config
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- ara.mk
+++ ara.mk
@@ -137,8 +137,8 @@ PRODUCT_COPY_FILES += \
     kernel/nokia/msm8610/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     kernel/nokia/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     kernel/nokia/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
-    kernel/nokia/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-    kernel/nokia/msm8610/drivers/staging/prima/firmware_bin/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
