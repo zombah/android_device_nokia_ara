KonstaT/android_device_zte_kis3

selinux: Drop BOARD_SEPOLICY_UNION
As suggested in the comments on
https://android-review.googlesource.com/#/c/141560/
drop BOARD_SEPOLICY_UNION and simplify the build_policy logic.
Union all files found under BOARD_SEPOLICY_DIRS.

Change-Id: I4214893c999c23631f5456cb1b8edd59771ef13b
Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

BoardConfig.mk
@@ -103,27 +103,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/zte/kis3/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    init_shell.te \
-    kernel.te \
-    location.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    platform_app.te \
-    property.te \
-    property_contexts \
-    rmt_storage.te \
-    system_server.te \
-    thermal-engine.te \
-    wcnss_service.te
+BOARD_SEPOLICY_DIRS += device/zte/kis3/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true

zombah/android_device_nokia_ara

Conflicts:
	BoardConfig.mk

BoardConfig.mk
@@ -138,26 +138,7 @@ BOARD_HAS_JANKY_BACKBUFFER := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    device/nokia/ara/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    init_shell.te \
-    kernel.te \
-    location.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    platform_app.te \
-    property.te \
-    property_contexts \
-    rmt_storage.te \
-    system_server.te \
-    thermal-engine.te \
-    wcnss_service.te
+BOARD_SEPOLICY_DIRS += device/nokia/ara/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true