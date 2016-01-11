1f195eee71bdbbd66bb156f0caef204bafb4af51
Misha Komarovskiy <zombah@gmail.com>
2016-01-12 02:50:58 +0300

    libshim: Add VectorImpl for Nokia camera blob
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -22,3 +22,17 @@ LOCAL_MODULE := libshim_rmt_storage
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# nokia_camera
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+     VectorImpl.c
+
+LOCAL_SHARED_LIBRARIES :=
+LOCAL_MODULE := libshim_nokia_camera
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
