b4267c1c14d38b202d98904180ef1a62463b3d3c
Misha Komarovskiy <zombah@gmail.com>
2016-03-18 13:25:45 +0300

    Revert "sepolicy: Remove platform_app at all"
    This reverts commit 0f327d0c4cdeec5c962728c5d5d8d0851b1259b1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,7 @@ BOARD_SEPOLICY_UNION += \
     mediaserver.te \
     mm-qcamerad.te \
     mpdecision.te \
+    platform_app.te \
     property.te \
     property_contexts \
     rmt_storage.te \
